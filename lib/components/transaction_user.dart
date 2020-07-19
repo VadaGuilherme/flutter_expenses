632e4ae9179eb97f389d1868cc496c756f3d7270
Guilherme Vada <mail@guilhermevada.com>
2020-07-18 22:06:31 -0300

    initial commit

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,36 @@
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/components/transaction_list.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/cupertino.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: '1',
+      title: 'Tenis novo',
+      value: 310.75,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'Camisa',
+      value: 85.75,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:expenses/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Minhas despesas')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Grafico'),
+              elevation: 5,
+            ),
+          ),
+          TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
